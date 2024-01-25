d6e6e3d11bea62ccd482a99c630093f2df8e3283
Thales Pinhiero <thales-pinheiro@hotmail.com>
2024-01-25 15:11:14 -0300

    almost done

--- pokedex.js
+++ pokedex.js
@@ -1,12 +1,40 @@
+//se pokedex filtrada estiver vazia
+//se >1 argumentos
+
+
+
+
 import pokedex from './pokedex.json' assert { type: 'json' }
+
+const types = ["GRASS","POISON","FIRE","WATER","FLYING","BUG","NORMAL","ELECTRIC","GROUND","FAIRY","FIGHTING","PSYCHIC","ROCK","STEEL","ICE","GHOST","DRAGON","DARK"];
 
 const importPokemons = document.querySelector("#import-pokemons");
 addPokeCards(pokedex);
 
 const filterBtns = document.querySelectorAll(".filter a");
 filterBtns.forEach(btn => {
-	btn.addEventListener("click", event => handleFilterBtns(event));
+	btn.addEventListener("click", event => handleBtns(event));
 })
+
+let searchBar = document.querySelector("form");
+searchBar.addEventListener("submit", e => {
+	e.preventDefault();
+
+	const search = document.querySelector("#search-bar").value;
+	handleSearch(captalizeFirst(search));
+})
+
+//Displays a "0 results found!" message bellow the form
+function noResultsMsg(){
+	const newH1 = document.createElement("h1");
+	newH1.appendChild(document.createTextNode("0 results found!"));
+	
+	const newDiv = document.createElement("div");
+	newDiv.classList.add("no-results");
+	
+	newDiv.appendChild(newH1);
+	document.body.appendChild(newDiv);
+}
 
 function addPokeCards(pokemonsToImport){
 	const template = document.querySelector("#poke-card");
@@ -23,29 +51,77 @@ function addPokeCards(pokemonsToImport){
 		newDiv.appendChild(clone);
 	})
 
-	importPokemons.appendChild(newDiv);
+	document.body.appendChild(newDiv);
 }
 
-function handleFilterBtns(a){
+//Captalizes only the first letter os the string
+function captalizeFirst(word){
+	return word.charAt(0).toUpperCase() + word.toLowerCase().slice(1);
+}
+
+function handleBtns(a){
 	const selection = a.target;
 	a.preventDefault();
 
-	const pokedexClone = JSON.parse(JSON.stringify(pokedex));
-	const filteredPokedex = pokedexClone.filter(pokemon => pokemon.type.includes(selection.getAttribute("data-filter")));
-	
-	deleteDivChildren(importPokemons);
-	
 	if(selection.getAttribute("data-filter") == "All"){
+		clearDisplayArea();
 		addPokeCards(pokedex);
 	}
 	else{
+		console.log("Invalid command\n");
+	}
+}
+
+function removeDivByAttbt(attbt){
+	const div = document.querySelector(attbt);
+	if(div != null){
+		div.remove();
+	}
+}
+
+function clearDisplayArea(){
+	removeDivByAttbt(".no-results");
+	removeDivByAttbt(".pokemons");
+}
+
+function handleSearch(search){
+	const pokedexClone = JSON.parse(JSON.stringify(pokedex));
+
+	var filteredPokedex = new String;
+	let pokeId;
+
+	//Runs if the search input is a type
+	if(types.includes(search.toUpperCase())){
+		filteredPokedex = pokedexClone.filter(pokemon => pokemon.type.includes(search));
+	}
+
+	//Runs if the search input is a name or an numeric id
+	else{
+		for(let pokemon of pokedex){
+			if(pokemon.id == search){
+				pokeId = pokemon.id;
+				break;
+			}
+			else if(pokemon.name.english == search){
+				pokeId = pokemon.id;
+				break;
+			}
+		}
+		filteredPokedex = pokedexClone.filter(pokemon => pokemon.id == pokeId);
+	}
+
+	clearDisplayArea();
+
+	//Check if search result is empty
+	if(filteredPokedex == ""){
+		noResultsMsg();
+	}
+	
+	else{
 		addPokeCards(filteredPokedex);
 	}
-}
 
-function deleteDivChildren(div){
-	while(div.firstChild){
-		div.removeChild(div.firstChild);
-	}
-}
+	//Clear search bar
+	document.querySelector("#search-bar").value = '';
 
+}
